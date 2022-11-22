dacf176edfd8f913aad7f82225a46c181b0aff2e
Def <deferenseo@gmail.com>
2022-11-22 14:53:34 +0300

    add menu appearing+navigation to blocks

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,31 @@
 import '../styles/reset.scss';
 import '../styles/mixins.scss';
 import '../styles/style.scss';
+
+const classes = {
+    opened: 'opened',
+};
+
+const header = document.querySelector('.header');
+const menuLink = document.querySelectorAll('.menu-link');
+const menuButton = document.querySelector('.header-menu__button');
+
+
+const toggleMenu = () => header.classList.toggle(classes.opened);
+
+const scrollToSection = (e) => {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+
+    if (!href && !href.startsWith('#')) {
+        return;
+    }
+    const section = href.slice(1);
+    const top = document.getElementById(section).offsetTop;
+    window.scrollTo({ top, behavior: 'smooth' });
+};
+
+
+menuButton.addEventListener('click', toggleMenu);
+
+menuLink.forEach((link) => link.addEventListener('click', scrollToSection));
